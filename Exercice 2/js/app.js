626b59e4d759ddfd1ce852f63923195fe71114b2
Julien <papao1@hotmail.fr>
2017-07-05 16:48:01 +0200

    Exercice 2 terminé

--- Exercice 2/js/app.js
+++ Exercice 2/js/app.js
@@ -1,6 +1,10 @@
 
 console.log('hello world');  
 
-var text = document.createTextNode('Ponny Land');
+var text = document.createTextNode('Hello world');
 var child = document.getElementById('addText');
 child.appendChild(text);
+
+var element = document.getElementById('remove');
+element.parentNode.removeChild(element);
+
