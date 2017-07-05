885edb82d45ca2609c244c02f48619e0dd92445c
Julien <papao1@hotmail.fr>
2017-07-05 11:33:03 +0200

    Exercice 1 tableau crée

--- array/app.js
+++ array/app.js
@@ -1,1 +1,8 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+array[2];
+array[3];
+
+// Exercice 1
+
+var array = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
